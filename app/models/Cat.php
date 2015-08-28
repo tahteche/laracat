42b61df1ced171d7df8ee80b4428aa5c46b230f8
Tah Teche <tahteche@gmail.com>
2015-08-28 08:52:23 +0100

    create model for cats

--- app/models/Cat.php
+++ app/models/Cat.php
@@ -6,4 +6,16 @@
     {
       return $this->belongsTo('Breed');
     }
+
+    // public function getDates() {
+    // return array('date_of_birth', 'created_at', 'updated_at');
+    // }
+
+    public function owner(){
+      return $this->belongsTo('User');
+    }
+
+    public function isOwnedBy(User $user){
+      return $this->owner === $user->id;
+    }
   }
