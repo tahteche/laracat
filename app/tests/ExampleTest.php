55f549df82821ec7480c763049cdd680e3d8c790
Tah Teche <tahteche@gmail.com>
2015-10-20 09:07:17 +0100

    create unit test for the application

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -1,6 +1,44 @@
 <?php
 
+
 class ExampleTest extends TestCase {
+
+	public function setUp() {
+		parent::setUp();
+		Artisan::call('migrate');
+		$this->seed();
+		Route::enableFilters();
+	}
+
+	public function testHomePageRedirection() {
+		$this->call('GET', '/'); //What does call() do?
+		$this->assertRedirectedTo('cats');
+
+	}
+
+	public function testVisitorIsRedirected() {
+		$this->call('GET', '/cats/create');
+		$this->assertRedirectedTo('login');
+	}
+
+
+/*This Test Fails with MassAssignmentException unless I 
+put:
+
+Artisan::call('migrate');
+		$this->seed();
+
+Find out why
+*/
+	public function testLoggedInUserCanCreateCat() {
+		$user = new User(array('name' => 'John Doe', 
+													 'is_admin' => false));
+		$this->be($user);
+		$this->call('GET', '/cats/create');
+		$this->assertResponseOk();
+
+	}
+
 
 	/**
 	 * A basic functional test example.
@@ -9,7 +47,7 @@ class ExampleTest extends TestCase {
 	 */
 	public function testBasicExample()
 	{
-		$crawler = $this->client->request('GET', '/');
+		$crawler = $this->client->request('GET', 'http://localhost:8000/cats');
 
 		$this->assertTrue($this->client->getResponse()->isOk());
 	}
